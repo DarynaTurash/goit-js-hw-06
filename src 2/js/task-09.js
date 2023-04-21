ebf3817f612bb8714c95faefb3b3313d09abd7c6
DarynaTurash <daryna.turash@gmail.com>
2023-04-22 01:30:11 +0300

    script 9 was done

--- src 2/js/task-09.js
+++ src 2/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColorRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector('body');
+const nameOfColorRef = document.querySelector(".color");
+
+buttonChangeColorRef.addEventListener("click", onChangeBackground);
+
+function onChangeBackground(getRandomColor) {
+  bodyRef.style.background = getRandomHexColor();
+  nameOfColorRef.textContent = bodyRef.style.background;
+}
