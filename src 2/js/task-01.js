ffd5e116856c721bd67675b04a386686222d8751
DarynaTurash <daryna.turash@gmail.com>
2023-04-22 02:24:45 +0300

    script 8 was done: script 1 was uodated;

--- src 2/js/task-01.js
+++ src 2/js/task-01.js
@@ -1,16 +1,10 @@
 const allItemsRef = document.querySelectorAll('li.item');
 console.log(`Number of categories: ${allItemsRef.length}`);
 
-const namesOfCategoriesRef = document.querySelectorAll('h2');
-const listsInEveryCategoryRef = document.querySelectorAll('.item > ul');
-
-namesOfCategoriesRef.forEach(name => {
-    console.log(`Category: ${name.textContent}`);
-});
-
-listsInEveryCategoryRef.forEach(list => {
-    console.log(`Elements: ${list.childElementCount}`);
-});
+allItemsRef.forEach(titleAndAmount => {
+    console.log(`Category: ${titleAndAmount.firstElementChild.textContent}`);
+    console.log(`Elements: ${titleAndAmount.lastElementChild.querySelectorAll("li").length}`);
+})
 
 
 
--- src 2/js/task-08.js
+++ src 2/js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(".login-form");
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    
+    if(formElements.email.value === "" || formElements.password.value === "") {
+        alert("Усі поля форми повинні бути заповненні!");
+    }
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+
+    formRef.reset();
+};
+
+
 
